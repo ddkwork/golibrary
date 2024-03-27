b4e78bdb8bbaa33606ae359e51c43364a3e0a9a9
Admin <2762713521@qq.com>
2024-03-27 18:38:19 +0800

    OrderedMap add EditorData

---
+++ NodeProvider.go
@@ -0,0 +1,36 @@
+package golibrary
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+type NodeProvider[T any] interface {
+	fmt.Stringer
+	UUID() uuid.UUID
+	Clone() T
+	Kind() string
+	Container() bool
+	Parent() T
+	SetParent(parent T)
+	HasChildren() bool
+	NodeChildren() []T
+	SetChildren(children []T)
+	Enabled() bool
+	Open() bool
+	Depth() int
+	SetOpen(open bool)
+	//CellData(columnID int, data *CellData)
+	FillWithNameableKeys(m map[string]string)
+	ApplyNameableKeys(m map[string]string)
+}
+
+type EditorData[T any] interface {
+	CopyFrom(from T)
+	ApplyTo(to T)
+}
+
+func AsNode[T any](in T) NodeProvider[T] {
+	return any(in).(NodeProvider[T])
+}
--- orderedmap/orderedmap.go
+++ orderedmap/orderedmap.go
@@ -70,7 +70,6 @@ func (m *OrderedMap[K, V]) Update(k K, v V) { // todo test
 			Key:   k,
 			Value: v,
 		}
-		return
 	}
 }
 
@@ -263,10 +262,19 @@ func (m *OrderedMap[K, V]) InsertBefore(key K, val V) {
 	m.l.InsertBefore(pair[K, V]{Key: key, Value: val}, m.m[key])
 }
 
+//var _ golibrary.EditorData[any] = (*OrderedMap[comparable, any])(nil)
+
 func (m *OrderedMap[K, V]) CopyFrom(from *OrderedMap[K, V]) {
 	for _, kv := range from.List() {
 		m.Set(kv.Key, kv.Value) // 注意这里不检测是否存在，如果存在key则会覆盖刷新
 	}
+}
+
+func (m *OrderedMap[K, V]) ApplyTo(to *OrderedMap[K, V]) {
+	for _, p := range m.List() {
+		to.Set(p.Key, p.Value)
+	}
+	//*to = *m
 }
 
 func (m *OrderedMap[K, V]) String() string {
