bae5abd5474497e4f006c893f29d1d750b109537
Admin <2762713521@qq.com>
2024-07-11 08:31:18 +0800

    add CopyFS

--- mylog/safeStream.gen.go
+++ mylog/safeStream.gen.go
@@ -10,6 +10,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/fs"
 	"math/big"
 	"math/rand"
 	"os"
@@ -737,6 +738,52 @@ func CurrentDirName(path string) (currentDirName string) {
 		return BaseName(filepath.Dir(path))
 	}
 	return split[len(split)-1]
+}
+
+// CopyFS copies the file system fsys into the directory dir,
+// creating dir if necessary.
+//
+// Newly created directories and files have their default modes
+// where any bits from the file in fsys that are not part of the
+// standard read, write, and execute permissions will be zeroed
+// out, and standard read and write permissions are set for owner,
+// group, and others while retaining any existing execute bits from
+// the file in fsys.
+//
+// Symbolic links in fsys are not supported, a *PathError with Err set
+// to ErrInvalid is returned on symlink.
+//
+// Copying stops at and returns the first error encountered.
+func CopyFS(dir string, fsys fs.FS) error {
+	return fs.WalkDir(fsys, ".", func(path string, d fs.DirEntry, err error) error {
+		if !filepath.IsLocal(path) {
+			return err
+		}
+		newPath := filepath.Join(dir, path)
+		if d.IsDir() {
+			return os.MkdirAll(newPath, 0777)
+		}
+
+		// TODO(panjf2000): handle symlinks with the help of fs.ReadLinkFS
+		//   once https://go.dev/issue/49580 is done.
+		//  we also need filepathlite.IsLocal from https://go.dev/cl/564295.
+		if !d.Type().IsRegular() {
+			return &os.PathError{Op: "CopyFS", Path: path, Err: os.ErrInvalid}
+		}
+
+		r := Check2(fsys.Open(path))
+
+		defer r.Close()
+		info := Check2(r.Stat())
+
+		w := Check2(os.OpenFile(newPath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666|info.Mode()&0777))
+
+		if _ := Check2(io.Copy(w, r)); err != nil {
+			w.Close()
+			return &os.PathError{Op: "Copy", Path: newPath, Err: err}
+		}
+		return w.Close()
+	})
 }
 
 func CopyDir(dst, src string) {
