2c45360d571c39326743cad4ed6fcf7c254b9323
Admin <2762713521@qq.com>
2025-02-28 11:53:50 +0800

    dev

--- stream/image.go
+++ stream/image.go
@@ -25,20 +25,20 @@ import (
 //	}.Layout(gtx)
 //}
 
-func LoadImage(fileName string) image.Image {
-	file := mylog.Check2(os.ReadFile(fileName))
+func LoadImage(fileName string) image.Image { //一般而言，我们使用embed
+	b := mylog.Check2(os.ReadFile(fileName))
 	var img image.Image
 	switch filepath.Ext(fileName) {
 	case ".png":
-		img = mylog.Check2(png.Decode(bytes.NewReader(file)))
+		img = mylog.Check2(png.Decode(bytes.NewReader(b)))
 	case ".jpg", ".jpeg":
-		img = mylog.Check2(jpeg.Decode(bytes.NewReader(file)))
+		img = mylog.Check2(jpeg.Decode(bytes.NewReader(b)))
 	case ".gif":
-		img = mylog.Check2(gif.Decode(bytes.NewReader(file)))
+		img = mylog.Check2(gif.Decode(bytes.NewReader(b)))
 	case ".ico":
-		img = mylog.Check2(ico.Decode(bytes.NewReader(file)))
+		img = mylog.Check2(ico.Decode(bytes.NewReader(b)))
 	case ".bmp":
-		img = mylog.Check2(bmp.Decode(bytes.NewReader(file)))
+		img = mylog.Check2(bmp.Decode(bytes.NewReader(b)))
 	//case ".svg": //svg的话是giosvg直接解码元数据实现layout方法渲染
 	default:
 		mylog.Check("unsupported image format")
