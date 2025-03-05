eab24c25cdf91256359bfbd9c4a98a1e0fa430fe
Admin <2762713521@qq.com>
2025-03-06 00:07:53 +0800

    dev

--- stream/UpdateModsByWorkSpace.go
+++ stream/UpdateModsByWorkSpace.go
@@ -2,8 +2,10 @@ package stream
 
 import (
 	"github.com/ddkwork/golibrary/safemap"
+	"golang.org/x/mod/modfile"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strconv"
 	"strings"
 	"sync"
@@ -27,137 +29,34 @@ var skips = []string{
 // git ls-remote https://github.com/ddkwork/toolbox refs/heads/master
 // git ls-remote https://github.com/gioui/gio refs/heads/main
 // 但是要传递本地的仓库目录，太麻烦了
-func GetLastCommitHash(repositoryName string) string {
-	//理论上获取到hash再使用模块代理，这样才刷新的快？
-	//或者使用action得到hash先？
-
-	mylog.Check(os.Setenv("GOPROXY", "direct")) // 如果模块代理导致获取到的不是最新的提交哈希那么需要禁用模块代理，最可靠的方式是 GetLastCommitHashLocal
-	defer mylog.Check(os.Setenv("GOPROXY", "https://goproxy.cn,direct"))
-
-	//defer RunCommand("go env -w GOPROXY=https://goproxy.cn,direct")
-	s := RunCommand("git ls-remote " + repositoryName + " refs/heads/master").Output.String()
-	for hash := range strings.FieldsSeq(s) {
-		return hash
-	}
-	s = RunCommand("git ls-remote " + repositoryName + " refs/heads/main").Output.String()
-	for hash := range strings.FieldsSeq(s) {
-		return hash
-	}
-	panic("no commit hash found in master and main branch")
-	/*
-		# 获取最新提交哈希
-		$hash = (git ls-remote https://github.com/ddkwork/toolbox refs/heads/master).Split("`t")[0]
-		# 带哈希安装
-		go get -x "github.com/ddkwork/toolbox@$hash"
-
-		powershell:
-			$env:GOPROXY="direct"; go get -x github.com/ddkwork/toolbox@$(git ls-remote https://github.com/ddkwork/toolbox refs/heads/master | ForEach-Object { $_.Split()[0] })
-
-	*/
-}
-
-type ModInfo struct {
-	ModName       string //gioui.org
-	RepositoryUrl string //https://github.com/gioui/gio
-	repositoryDir string //GetLastCommitHashLocal
-	Hash          string //GetLastCommitHash or GetLastCommitHashLocal
-	UpdateCommand string //go get -x gioui.org@hash
-}
-
-var m = safemap.NewOrdered[string, ModInfo](func(yield func(string, ModInfo) bool) {
-	yield("https://github.com/gioui/gio", ModInfo{
-		ModName:       "gioui.org",
-		RepositoryUrl: "https://github.com/gioui/gio",
-		repositoryDir: "",
-		Hash:          "",
-		UpdateCommand: "",
-	})
-	yield("https://github.com/gioui/gio/cmd", ModInfo{
-		ModName:       "gioui.org/cmd",
-		RepositoryUrl: "https://github.com/gioui/gio/cmd",
-		repositoryDir: "",
-		Hash:          "",
-		UpdateCommand: "",
-	})
-	yield("https://github.com/gioui/gio/example", ModInfo{
-		ModName:       "gioui.org/example",
-		RepositoryUrl: "https://github.com/gioui/gio/example",
-		repositoryDir: "",
-		Hash:          "",
-		UpdateCommand: "",
-	})
-	yield("https://github.com/gioui/gio/x", ModInfo{
-		ModName:       "gioui.org/x",
-		RepositoryUrl: "https://github.com/gioui/gio/x",
-		repositoryDir: "",
-		Hash:          "",
-		UpdateCommand: "",
-	})
-	yield("https://github.com/oligo/gvcode", ModInfo{
-		ModName:       "github.com/oligo/gvcode",
-		RepositoryUrl: "https://github.com/oligo/gvcode",
-		repositoryDir: "",
-		Hash:          "",
-		UpdateCommand: "",
-	})
-	yield("https://github.com/ddkwork/golibrary", ModInfo{
-		ModName:       "github.com/ddkwork/golibrary",
-		RepositoryUrl: "https://github.com/ddkwork/golibrary",
-		repositoryDir: "",
-		Hash:          "",
-		UpdateCommand: "",
-	})
-	yield("https://github.com/ddkwork/ux", ModInfo{
-		ModName:       "github.com/ddkwork/ux",
-		RepositoryUrl: "https://github.com/ddkwork/ux",
-		repositoryDir: "",
-		Hash:          "",
-		UpdateCommand: "",
-	})
-	yield("https://github.com/google/go-cmp", ModInfo{
-		ModName:       "github.com/google/go-cmp",
-		RepositoryUrl: "https://github.com/google/go-cmp",
-		repositoryDir: "",
-		Hash:          "",
-		UpdateCommand: "",
-	})
-	yield("https://github.com/ddkwork/app", ModInfo{
-		ModName:       "github.com/ddkwork/app",
-		RepositoryUrl: "https://github.com/ddkwork/app",
-		repositoryDir: "",
-		Hash:          "",
-		UpdateCommand: "",
-	})
-	yield("https://github.com/ddkwork/toolbox", ModInfo{
-		ModName:       "github.com/ddkwork/toolbox",
-		RepositoryUrl: "https://github.com/ddkwork/toolbox",
-		repositoryDir: "",
-		Hash:          "",
-		UpdateCommand: "",
-	})
-	yield("https://github.com/ddkwork/unison", ModInfo{
-		ModName:       "github.com/ddkwork/unison",
-		RepositoryUrl: "https://github.com/ddkwork/unison",
-		repositoryDir: "",
-		Hash:          "",
-		UpdateCommand: "",
-	})
-	yield("https://github.com/ebitengine/purego", ModInfo{
-		ModName:       "github.com/ebitengine/purego",
-		RepositoryUrl: "https://github.com/ebitengine/purego",
-		repositoryDir: "",
-		Hash:          "",
-		UpdateCommand: "",
-	})
-	yield("https://github.com/saferwall/pe", ModInfo{
-		ModName:       "github.com/saferwall/pe",
-		RepositoryUrl: "https://github.com/saferwall/pe",
-		repositoryDir: "",
-		Hash:          "",
-		UpdateCommand: "",
-	})
-
-})
+//func GetLastCommitHash(repositoryName string) string {
+//	//理论上获取到hash再使用模块代理，这样才刷新的快？
+//	//或者使用action得到hash先？
+//
+//	mylog.Check(os.Setenv("GOPROXY", "direct")) // 如果模块代理导致获取到的不是最新的提交哈希那么需要禁用模块代理，最可靠的方式是 GetLastCommitHashLocal
+//	defer mylog.Check(os.Setenv("GOPROXY", "https://goproxy.cn,direct"))
+//
+//	//defer RunCommand("go env -w GOPROXY=https://goproxy.cn,direct")
+//	s := RunCommand("git ls-remote " + repositoryName + " refs/heads/master").Output.String()
+//	for hash := range strings.FieldsSeq(s) {
+//		return hash
+//	}
+//	s = RunCommand("git ls-remote " + repositoryName + " refs/heads/main").Output.String()
+//	for hash := range strings.FieldsSeq(s) {
+//		return hash
+//	}
+//	panic("no commit hash found in master and main branch")
+//	/*
+//		# 获取最新提交哈希
+//		$hash = (git ls-remote https://github.com/ddkwork/toolbox refs/heads/master).Split("`t")[0]
+//		# 带哈希安装
+//		go get -x "github.com/ddkwork/toolbox@$hash"
+//
+//		powershell:
+//			$env:GOPROXY="direct"; go get -x github.com/ddkwork/toolbox@$(git ls-remote https://github.com/ddkwork/toolbox refs/heads/master | ForEach-Object { $_.Split()[0] })
+//
+//	*/
+//}
 
 func GetLastCommitHashLocal(repositoryName, repositoryDir string) string {
 	originPath := mylog.Check2(os.Getwd())
@@ -167,32 +66,38 @@ func GetLastCommitHashLocal(repositoryName, repositoryDir string) string {
 	return hash
 }
 
-func UpdateDependencies() { //模块代理刷新的不及时，需要禁用代理
-	mylog.Check(os.Setenv("GOPROXY", "direct"))
-	for s := range strings.Lines(`
-     go get -x gioui.org@main
-	 go get -x gioui.org/cmd@main
-	 go get -x gioui.org/example@main
-	 go get -x gioui.org/x@main
-	 go get -x github.com/oligo/gvcode@main
-	 go get -x github.com/ddkwork/golibrary@master
-	 go get -x github.com/ddkwork/ux@master
-	 go get -x github.com/google/go-cmp@master
-	 go get -x github.com/ddkwork/app@master
-	 go get -x github.com/ddkwork/toolbox@master
-	 go get -x github.com/ddkwork/unison@master
-	 go get -x github.com/ebitengine/purego@main
-	 go get -x github.com/saferwall/pe@main
-	 ::go get -u -x all
-	 go mod tidy
+func ParseGoMod() *safemap.M[string, string] {
+	path := "go.mod"
+	f := mylog.Check2(modfile.Parse(path, mylog.Check2(os.ReadFile(path)), nil))
+	return safemap.NewOrdered[string, string](func(yield func(string, string) bool) {
+		for _, req := range f.Require {
+			yield(req.Mod.Path, req.Mod.Version)
+		}
+	})
+}
 
-	go install mvdan.cc/gofumpt@latest
-	gofumpt -l -w .
-	//go install honnef.co/go/tools/cmd/staticcheck@latest
-	//staticcheck ./...
-	//go test -v -race -coverprofile=coverage.txt -covermode=atomic ./...
+func GetDesktopDir() string {
+	// 获取用户主目录
+	homeDir := mylog.Check2(os.UserHomeDir())
+	// 根据操作系统处理路径
+	switch runtime.GOOS {
+	case "windows", "darwin":
+		// Windows和macOS直接拼接Desktop
+		return filepath.Join(homeDir, "Desktop")
+	case "linux":
+		// Linux优先检查XDG环境变量
+		if xdgDir := os.Getenv("XDG_DESKTOP_DIR"); xdgDir != "" {
+			return xdgDir
+		}
+		// 默认使用主目录下的Desktop
+		return filepath.Join(homeDir, "Desktop")
+	default:
+		panic("unsupported platform")
+	}
+}
 
-`) {
+func UpdateDependencies() { //模块代理刷新的不及时，需要禁用代理,已经使用clone仓库远程完成更新
+	for s := range ReadFileToLines(filepath.Join(GetDesktopDir(), "dep.txt")) { //因为要经常更新，我们不embed
 		s = strings.TrimSpace(s)
 		if strings.HasPrefix(s, "::") || strings.HasPrefix(s, "//") || s == "" {
 			continue
@@ -246,6 +151,40 @@ func updateModsByWorkSpace(isUpdateAll bool) {
 	}()
 	mylog.Check(g.Wait())
 	mylog.Success("all work finished")
+}
+
+func updateDependencies() { //模块代理刷新的不及时，需要禁用代理,已经使用clone仓库远程完成更新
+	mylog.Check(os.Setenv("GOPROXY", "direct"))
+	for s := range strings.Lines(`
+     go get -x gioui.org@main
+	 go get -x gioui.org/cmd@main
+	 go get -x gioui.org/example@main
+	 go get -x gioui.org/x@main
+	 go get -x github.com/oligo/gvcode@main
+	 go get -x github.com/ddkwork/golibrary@master
+	 go get -x github.com/ddkwork/ux@master
+	 go get -x github.com/google/go-cmp@master
+	 go get -x github.com/ddkwork/app@master
+	 go get -x github.com/ddkwork/toolbox@master
+	 go get -x github.com/ddkwork/unison@master
+	 go get -x github.com/ebitengine/purego@main
+	 go get -x github.com/saferwall/pe@main
+	 ::go get -u -x all
+	 go mod tidy
+
+	go install mvdan.cc/gofumpt@latest
+	gofumpt -l -w .
+	//go install honnef.co/go/tools/cmd/staticcheck@latest
+	//staticcheck ./...
+	//go test -v -race -coverprofile=coverage.txt -covermode=atomic ./...
+
+`) {
+		s = strings.TrimSpace(s)
+		if strings.HasPrefix(s, "::") || strings.HasPrefix(s, "//") || s == "" {
+			continue
+		}
+		RunCommand(s)
+	}
 }
 
 //type Cache struct {
