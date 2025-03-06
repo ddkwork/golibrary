21f236d6089106acd3e1306390c53f87ef706e68
Admin <2762713521@qq.com>
2025-03-06 16:33:43 +0800

    dev

--- staticcheck.go
+++ staticcheck.go
@@ -15,5 +15,5 @@ func UpdateSelf() {
 	hash := stream.GetLastCommitHashLocal("D:\\workspace\\workspace\\golibrary")
 	stream.RunCommand("go get -v -x github.com/ddkwork/golibrary@" + hash)
 	stream.RunCommand("go mod tidy")
-	// 更新桌面的dep.txt
+	// 更新桌面的go.mod
 }
