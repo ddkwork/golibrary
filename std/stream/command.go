a45add66f5df66b536784ee8a259a881945b09db
Admin <2762713521@qq.com>
2025-06-05 21:24:05 +0800

    dev

--- std/stream/command.go
+++ std/stream/command.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"context"
 	"io"
-	"os"
 	"os/exec"
 	"path/filepath"
 	"regexp"
@@ -37,7 +36,7 @@ func runCommand(dir string, arg ...string) (stdOut *GeneratedFile) {
 		handleWait func() //merge exit code.and error
 	}
 	cmdKey := "command" //fast the key is clang target file path
-	fast := false
+	fast := true
 	stdOut = NewGeneratedFile()
 	stderr := NewGeneratedFile()
 
@@ -57,9 +56,10 @@ func runCommand(dir string, arg ...string) (stdOut *GeneratedFile) {
 		init: func() {
 			binaryPath := arg[0]
 			switch binaryPath { //todo add more need fast model
-			case "clang", "clang-format":
+			//case "clang", "clang-format":
+			case "ping":
 				cmdKey = filepath.Base(arg[len(arg)-1])
-				fast = true
+				fast = false
 			}
 
 			cmd = exec.CommandContext(ctx, binaryPath, arg[1:]...)
@@ -68,18 +68,10 @@ func runCommand(dir string, arg ...string) (stdOut *GeneratedFile) {
 			mylog.Success(cmdKey, cmd.String())
 
 			stdoutPipe = mylog.Check2(cmd.StdoutPipe())
-			if isRunningOnGitHubActions() {
-				cmd.Stderr = os.Stderr
-				return
-			}
-
 			stderrPipe = mylog.Check2(cmd.StderrPipe())
 		},
 		fastModel: func() {
 			mylog.Check2(stdOut.ReadFrom(stdoutPipe))
-			if isRunningOnGitHubActions() {
-				return
-			}
 			mylog.Check2(stderr.ReadFrom(stderrPipe))
 		},
 		slowModel: func() {
@@ -158,10 +150,6 @@ func runCommand(dir string, arg ...string) (stdOut *GeneratedFile) {
 	stdOut.Reset()
 	stdOut.WriteString(ss)
 	return
-}
-
-func isRunningOnGitHubActions() bool {
-	return os.Getenv("GITHUB_ACTIONS") == "true"
 }
 
 func trimTrailingEmptyLines(s string) string {
