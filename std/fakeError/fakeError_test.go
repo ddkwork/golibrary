045df36e15feba211777550fee86fd354e385fd8
Admin <2762713521@qq.com>
2026-03-21 20:57:32 +0800

    test(fakeError): 添加新测试用例

--- std/fakeError/fakeError_test.go
+++ std/fakeError/fakeError_test.go
@@ -15,6 +15,60 @@ import (
 )
 
 func TestName(t *testing.T) {
+	bad := `package stream
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/ddkwork/golibrary/std/mylog"
+)
+
+// GetAvailablePort 获取可用端口
+func GetAvailablePort() int {
+	listener := mylog.Check2(net.ListenTCP("tcp", mylog.Check2(net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:0", "0.0.0.0")))))
+	defer func() { mylog.Check(listener.Close()) }()
+	return listener.Addr().(*net.TCPAddr).Port
+}
+
+// IsPortAvailable 判断端口是否可以（未被占用）
+func IsPortAvailable(port int) bool {
+	address := fmt.Sprintf("%s:%d", "0.0.0.0", port)
+	listener := mylog.Check2(net.Listen("tcp", address))
+
+	defer mylog.Check(listener.Close())
+	return true
+}
+`
+	ok := `package stream
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/ddkwork/golibrary/std/mylog"
+)
+
+// GetAvailablePort 获取可用端口
+func GetAvailablePort() int {
+	listener := mylog.Check2(net.ListenTCP("tcp", mylog.Check2(net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:0", "0.0.0.0")))))
+	defer func() { mylog.Check(listener.Close()) }()
+	return listener.Addr().(*net.TCPAddr).Port
+}
+
+// IsPortAvailable 判断端口是否可以（未被占用）
+func IsPortAvailable(port int) bool {
+	address := fmt.Sprintf("%s:%d", "0.0.0.0", port)
+	listener := mylog.Check2(net.Listen("tcp", address))
+
+	defer mylog.Check(listener.Close())
+	return true
+}`
+	assert.Equal(t, ok, get("testName", bad))
+
+}
+
+func Test111(t *testing.T) {
 	bad := `package udp
 
 import "github.com/ddkwork/golibrary/std/mylog"
